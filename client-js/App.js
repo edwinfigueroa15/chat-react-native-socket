08ef7496a2045a476bdaade39c627dd63e16a22e
Edwin Figueroa <edwinfigueroa.dev@gmail.com>
2025-07-22 16:21:05 -0500

    Dependencias base

--- client-js/App.js
+++ client-js/App.js
@@ -1,12 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
+import { NativeBaseProvider } from 'native-base';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <NativeBaseProvider>
+        <View style={styles.container}>
+          <Text>Chat App</Text>
+        </View>
+      </NativeBaseProvider>
+    </NavigationContainer>
   );
 }
 
